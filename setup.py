9b90103059b11e0e01d5db96b1b44907722a80ba
Shankar Ganesh Jayaraman <shangan.23@gmail.com>
2020-04-12 22:43:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="similar-sentences",
+    version="0.0.1",
+    author="Shankar Ganesh Jayaraman",
+    author_email="shangan.23@gmail.com",
+    description="Similar sentence prediction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shangan23/similar-sentences",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "sentence-transformers"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
