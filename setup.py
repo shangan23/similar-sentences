348fb4d2b4c6d1722808f52d13b2853c86015549
Shankar Ganesh Jayaraman <shangan.23@gmail.com>
2020-04-12 23:20:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SimilarSentences",
-    version="0.0.1",
+    version="0.0.2",
     author="Shankar Ganesh Jayaraman",
     author_email="shangan.23@gmail.com",
     description="Similar sentence prediction",
