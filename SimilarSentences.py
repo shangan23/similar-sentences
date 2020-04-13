c3a905bd12b34047dec6a9376e628f7d8c0f7057
Shankar Ganesh Jayaram <shankar@coremobileinc.com>
2020-04-13 17:42:26 +0530

    Prediction method added

--- SimilarSentences.py
+++ SimilarSentences.py
@@ -1,42 +1,97 @@
 import numpy as np
 import scipy.spatial
 import logging
+import json
+import nltk
+import zipfile
+import os
 from sentence_transformers import SentenceTransformer, LoggingHandler
 
-class SimilarSentences():
 
-    def __init__(self):
+class SimilarSentences(object):
+
+    def __init__(self, path):
+        dir_path = os.getcwd()
+        model_path = dir_path+'/model/'
+        if(zipfile.is_zipfile(path)):
+            with zipfile.ZipFile(path, 'r') as zip_ref:
+                zip_ref.extractall(model_path)
+                if(os.path.isdir(model_path+'0_BERT') and os.path.isdir(model_path+'1_Pooling') and os.path.isfile(model_path+'vector.npy')):
+                    print('Model validation OK...')
+                else:
+                    exit('Model file is not valid... exiting...')
+            self.model_path = model_path
+        else:
+            exit('Model file is not in .zip format')
 
     def get_path(self):
+        _vector_file = 'vector.npy'
+        _training_set = 'train.txt'
+        _files = {
+            'model': self.model_path,
+            'vector': self.model_path + _vector_file,
+            'training_set': self.model_path+_training_set
+        }
+        return _files
 
-    def train(self,sentences):
+    def get_sentences(self, text):
+        return nltk.sent_tokenize(text)
+
+    def predict(self, text, num_of_simlar_sentences, response="simple"):
         path = self.get_path()
-        vectorfile = 'vectorfile.npy'
-        np.set_printoptions(threshold=100)
-        logging.basicConfig(format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO,
-                    handlers=[LoggingHandler()])
-        model = SentenceTransformer('bert-base-nli-mean-tokens')
-        sentence_embeddings = model.encode(sentences)
-        model.save(path);
-        np.save(path+vectorfile, sentence_embeddings)
+        model = SentenceTransformer(path.get('model'))
+        given_text = self.get_sentences(text)
+        text_embedding = model.encode(given_text)
+        return self.output(given_text, text_embedding, num_of_simlar_sentences, response)
 
-    def download_model(self):
+    def output(self, text, embedding, closest_n, response):
 
-    def check_file(self):
-        
-    def predict(self,similar_to,num):
-        model = SentenceTransformer('/content/model_to_save')
-        sentences_file = np.load('VzSentenceVectors.npy')
-        vecs = np.stack(sentences_file)
-        query_embeddings = model.encode(sentences_to_find)
-        closest_n = num
-        for query, query_embedding in zip(queries, query_embeddings):
-            distances = scipy.spatial.distance.cdist([query_embedding], vecs, "cosine")[0]
-            results = zip(range(len(distances)), distances)
-            results = sorted(results, key=lambda x: x[1])
-            print("\nQuery:", query)
-            print("\nTop 5 most similar sentences in corpus:")
-            for idx, distance in results[0:closest_n]:
-                print(sentences[idx].strip(), "(Score: %.4f)" % (1-distance))
+        path = self.get_path()
+        sentences = open(path.get('training_set')).read().splitlines()
+        sentences_vector = np.load(path.get('vector'))
+        vectors = np.stack(sentences_vector)
+        output = {}
+        i = 0
+        while i < closest_n:
+            out = []
+            for query, sentence in zip(text, embedding):
+                distances = scipy.spatial.distance.cdist(
+                    [sentence], vectors, "cosine")[0]
+
+                results = zip(range(len(distances)), distances)
+                results = sorted(results, key=lambda x: x[1])
+                index = 0
+                for idx, distance in results[0:closest_n]:
+                    if(i == index):
+                        out.append(
+                            {"sentence": sentences[idx].strip(), "score": (1-distance)})
+                    index += 1
+                output[i] = out
+            i += 1
+        return self.json_output(output, response)
+
+    def json_output(self, data, response):
+        data = [*data.values()]
+        json_out = ''
+        if(response == "detailed"):
+            json_out = json.dumps(data)
+        elif(response == "simple"):
+            simple_data = []
+            for i in data:
+                str = ''
+                k = 0
+                for j in i:
+                    if(k == 0):
+                        str += j['sentence']
+                    else:
+                        str += ' '+j['sentence']
+                    k += 1
+                simple_data.append(str)
+            json_out = json.dumps(simple_data)
+        return json_out
+
+
+model = SimilarSentences('model_org.zip')
+text = 'Would you like to add TravelPass? The cost of TravelPass is $5/day for North America and $10/day for the rest of the world.'
+suggestions = model.predict(text, 2, "simple")
+print(suggestions)
