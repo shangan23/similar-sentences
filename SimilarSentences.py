ac808d83f52362e3019290c3069190ff0ff2b836
Shankar Ganesh Jayaram <shankar@coremobileinc.com>
2020-04-13 17:43:34 +0530

    Prediction method added

--- SimilarSentences.py
+++ SimilarSentences.py
@@ -89,9 +89,3 @@ class SimilarSentences(object):
                 simple_data.append(str)
             json_out = json.dumps(simple_data)
         return json_out
-
-
-model = SimilarSentences('model_org.zip')
-text = 'Would you like to add TravelPass? The cost of TravelPass is $5/day for North America and $10/day for the rest of the world.'
-suggestions = model.predict(text, 2, "simple")
-print(suggestions)
